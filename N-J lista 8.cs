b8d1bb50b524379c71fe7c8d872f7b61f8771bb1
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-10 18:24:44 -0300

    Create N-J lista 8.cs

---
+++ N-J lista 8.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("soma dos numeros de 1 a 100:");
+        int soma =0;
+        
+        for (int i = 0; i <= 100; i+=2)
+        {
+        	soma+=i;
+        }
+        Console.WriteLine("a soma dos numeros de 1 a 100 é "+soma);
+      
+        Console.WriteLine("Clique qualquer botão para encerrar...");
+        Console.ReadKey();
+    }
+}
