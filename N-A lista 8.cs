6261c5df30ba078a0fbdb83e7684380b68d57668
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-10 18:16:19 -0300

    Create N-A lista 8.cs

---
+++ N-A lista 8.cs
@@ -0,0 +1,16 @@
+using System;
+	
+
+class Program
+{
+   public static void Main()
+    {
+    	for (int i = 0; i < 100; i += 2)
+    	{
+    		Console.WriteLine(i);
+    		
+    	}
+        Console.WriteLine("Clique qualquer botão para encerrar...");
+        Console.ReadKey();
+    }
+}
